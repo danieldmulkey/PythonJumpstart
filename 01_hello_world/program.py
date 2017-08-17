adbf24d41dc53d9920fd0fa1ea999ca89a3ff4ae
danieldmulkey <danieldmulkey@gmail.com>
2017-08-17 12:52:29 -0700

    Initial commit

---
+++ 01_hello_world/program.py
@@ -0,0 +1,8 @@
+print('-'*20)
+# TODO: Center following text in output
+print(' '*4 + 'HELLO WORLD')
+print('-'*20)
+
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you ' + user_text
+print(greeting)
