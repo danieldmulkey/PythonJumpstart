90f64a07e56ba13852cab335cdffa83b3b682fc8
danieldmulkey <danieldmulkey@gmail.com>
2017-08-17 12:53:33 -0700

    Pre-video attempt at guess_number_game

---
+++ 02_guess_number_game/program.py
@@ -0,0 +1,22 @@
+import random
+
+print('-' * 35)
+print(' ' * 5, 'GUESS THE NUMBER APP')
+print('-' * 35)
+
+target = random.randint(0, 100)
+
+prompt = 'Guess a number between 0 and 100: '
+guess = int(input(prompt))
+
+while guess != target:
+    if guess > target:
+        fail = 'Sorry but {} is {} than the number'.format(guess, 'GREATER')
+        print(fail)
+    elif guess < target:
+        fail = 'Sorry but {} is {} than the number'.format(guess, 'LOWER')
+        print(fail)
+    guess = int(input(prompt))
+
+success = 'YES! You\'ve got it. The number was {}'.format(target)
+print(success)
