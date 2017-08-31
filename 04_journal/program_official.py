b6d2cf44e2c3b333f0e294b9298ae49088d81562
danieldmulkey <danieldmulkey@gmail.com>
2017-08-31 12:54:35 -0700

    Partway through video, more

--- 04_journal/program_official.py
+++ 04_journal/program_official.py
@@ -15,18 +15,18 @@ def print_header():
 
 def run_event_loop():
     print('What do you want to do with your journal?')
-    cmd = None
+    cmd = 'EMPTY'
     journal_name = 'default'
     journal_data = journal.load(journal_name)   # []  # list()
 
-    while cmd != 'x':
+    while cmd != 'x' and cmd:  # because empty string evaluates to False
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
         cmd = cmd.lower().strip()
         if cmd == 'l':
             list_entries(journal_data)
         elif cmd == 'a':
             add_entry(journal_data)
-        elif cmd != 'x':
+        elif cmd != 'x' and cmd:  # so empty command doesn't trigger
             print("Sorry, we don't understand {}".format(cmd))
 
     print('Done, goodbye.')
